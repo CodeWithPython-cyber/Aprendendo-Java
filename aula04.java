31258cfa224284125450c8130bdfefdb1e329885
Enzo Gabriel <enzogabsn@gmail.com>
2024-12-26 16:12:04 -0300

    aula04.java

---
+++ aula04.java
@@ -0,0 +1,8 @@
+// Programa que exibe "Olá, Mundo!" na tela
+// Autor: Enzo Gabriel Santos Nunes
+
+public class aula04 { // Declara uma classe chamada primeiroprograma
+        public static void main(String[] args) { // Declara um método chamado main
+        System.out.println("Olá, Mundo!"); // Exibe "Olá, Mundo!" na tela
+    }
+}
